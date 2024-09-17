a31b51482f9cf59b19e71f0e8b4415957f74a015
sikibam <128597320+sikibam@users.noreply.github.com>
2024-09-17 10:15:54 +0800

    Update and rename BufferedReader.java to BufferedReaderActivity.java

--- BufferedReaderActivity.java
+++ BufferedReaderActivity.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-public class BufferedReader {
+public class BufferedReaderActivity {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
